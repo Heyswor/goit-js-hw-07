821a26e83338d59be2d593f6daac1beb22349239
Heyswor <andreydymov93@gmail.com>
2022-09-15 18:06:12 +0300

    Revert "Homewrok 7-2"
    This reverts commit 0622e8e523940f2058373d29e5e962546eabec32.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,23 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from '../node_modules/simplelightbox/dist/simple-lightbox.esm.js'
 // Change code below this line
-console.log(SimpleLightbox);
+
 console.log(galleryItems);
-
-const galleryRef = document.querySelector(".gallery");
-
-const markup = galleryItems
-  .map(
-    (image) =>
-      `<div class="gallery__item"><a class="gallery__link" href="${image.original}" onclick="return false;"><img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300"> </a></div>`
-  )
-  .join("");
-galleryRef.insertAdjacentHTML("afterbegin", markup);
-
-
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionType: "attr",
-  captionsData: "alt",
-  captionPosition: "bottom",
-  captionDelay: 250,
-});
