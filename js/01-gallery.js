1fc6366c0650be3ed7a7fbbb9fc9318817e9ef4a
Heyswor <andreydymov93@gmail.com>
2022-09-15 19:02:14 +0300

    Revert "Revert "Homewrok 7-2""
    This reverts commit 821a26e83338d59be2d593f6daac1beb22349239.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,86 +5,53 @@ import { galleryItems } from "./gallery-items.js";
 // console.log(galleryItems);
 
 const galleryRef = document.querySelector(".gallery");
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img>
+    </div>
+`);
+const modalEl = instance.element();
 
 const markup = galleryItems
   .map(
     (image) =>
-      `<div class="gallery__item">
-        <a class="gallery__link" href="${image.original}" onclick="return false;">
-          <img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300">
-        </a>
-      </div>`
+      `<div class="gallery__item"><a class="gallery__link" href="${image.original}" onclick="return false;"><img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300"> </a></div>`
   )
   .join("");
 galleryRef.insertAdjacentHTML("afterbegin", markup);
 
-const imageRef = document.querySelectorAll(".gallery img");
-console.log(imageRef);
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <img src='' alt=''>
-//     </div>
-// `);
-
 const onImageClick = (event) => {
-  imageRef.forEach((image) => {
-    console.log(image);
-    const instance = basicLightbox.create(`
-    <div class="modal">
-        <img src='${image.getAttribute("data-source")}' alt=''>
-    </div>
-`);
-    instance.show(() => {});
+  const { target } = event;
+
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  galleryItems.map((element) => {
+    if (target.alt === element.description) {
+      instance.show(() => {
+        const bigImg = modalEl.querySelector("img");
+        bigImg.classList = "modalImg";
+        bigImg.src = target.dataset.source;
+        bigImg.alt = target.alt;
+        if (modalEl) {
+          modalEl.addEventListener("click", (event) => {
+            instance.close(() => {
+              event.target.src.remove();
+            });
+          });
+        }
+      });
+    }
   });
 };
 
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//     <img>
-//     </div>
-// `);
-// const modalEl = instance.element();
-
-// const markup = galleryItems
-//   .map(
-//     (image) =>
-//       `<div class="gallery__item"><a class="gallery__link" href="${image.original}" onclick="return false;"><img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300"> </a></div>`
-//   )
-//   .join("");
-// galleryRef.insertAdjacentHTML("afterbegin", markup);
-
-// const onImageClick = (event) => {
-//   const { target } = event;
-
-//   if (target.nodeName !== "IMG") {
-//     return;
-//   }
-
-//   galleryItems.map((element) => {
-//     if (target.alt === element.description) {
-//       instance.show(() => {
-//         const bigImg = modalEl.querySelector("img");
-//         bigImg.classList = "modalImg";
-//         bigImg.src = target.dataset.source;
-//         bigImg.alt = target.alt;
-//         if (modalEl) {
-//           modalEl.addEventListener("click", (event) => {
-//             instance.close(() => {
-//               event.target.src.remove();
-//             });
-//           });
-//         }
-//       });
-//     }
-//   });
-// };
-
-// document.addEventListener("keydown", (escape) => {
-//   if (escape.key === "Escape") {
-//     instance.close(() => {
-//       target.remove();
-//     });
-//   }
-// });
+document.addEventListener("keydown", (escape) => {
+  if (escape.key === "Escape") {
+    instance.close(() => {
+      target.remove();
+    });
+  }
+});
 
 galleryRef.addEventListener("click", onImageClick);
