6c2cd73569ef7156447d8f7aa9cfe3c2996666af
Heyswor <andreydymov93@gmail.com>
2022-09-16 21:21:42 +0300

    Homework 7-4

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,20 +26,64 @@ const onImageClick = (event) => {
       )}'>
     </div>
 `);
+      const escapeListener = (escape) => {        
+        if (escape.key === "Escape") {
+          document.removeEventListener("keydown", escapeListener);
+          instance.close();
+        }
+      };
       instance.show();
       const modalEl = instance.element();
       if (modalEl) {
         modalEl.addEventListener("click", (event) => {
           instance.close();
         });
-        document.addEventListener("keydown", (escape) => {
-          if (escape.key === "Escape") {
-            instance.close();
-          }
-        });
+        document.addEventListener("keydown", escapeListener);
       }
     }
   });
 };
 
 galleryRef.addEventListener("click", onImageClick);
+
+// const galleryRef = document.querySelector(".gallery");
+
+// const markup = galleryItems
+//   .map(
+//     (image) =>
+//       `<div class="gallery__item"><a class="gallery__link" href="${image.original}" onclick="return false;"><img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300"> </a></div>`
+//   )
+//   .join("");
+// galleryRef.insertAdjacentHTML("afterbegin", markup);
+
+// const imagesRef = document.querySelectorAll(".gallery img");
+
+// const onImageClick = (event) => {
+//   const { target } = event;
+
+//   imagesRef.forEach((image) => {
+//     if (image.getAttribute("alt") === target.alt) {
+//       const instance = basicLightbox.create(`
+//     <div class="modal">
+//     <img src='${image.getAttribute("data-source")}' alt='${image.getAttribute(
+//         "alt"
+//       )}'>
+//     </div>
+// `);
+//       instance.show();
+//       const modalEl = instance.element();
+//       if (modalEl) {
+//         modalEl.addEventListener("click", (event) => {
+//           instance.close();
+//         });
+//         document.addEventListener("keydown", (escape) => {
+//           if (escape.key === "Escape") {
+//             instance.close();
+//           }
+//         });
+//       }
+//     }
+//   });
+// };
+
+// galleryRef.addEventListener("click", onImageClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,7 @@ const galleryRef = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
     (image) =>
-      `<div class="gallery__item"><a class="gallery__link" href="${image.original}" onclick="return false;"><img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300"> </a></div>`
+      `<li class="gallery__item"><a class="gallery__link" href="${image.original}" onclick="return false;"><img class='gallery__image' src="${image.preview}" data-source="${image.original}" alt="${image.description}" width="300"> </a></li>`
   )
   .join("");
 galleryRef.insertAdjacentHTML("afterbegin", markup);
