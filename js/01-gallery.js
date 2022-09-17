98a9482438a40f3f892b66cf16d75b6f4720a5cd
Heyswor <andreydymov93@gmail.com>
2022-09-17 21:59:09 +0300

    Homework 7-5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,39 +12,60 @@ const markup = galleryItems
   .join("");
 galleryRef.insertAdjacentHTML("afterbegin", markup);
 
-const imagesRef = document.querySelectorAll(".gallery img");
-
 const onImageClick = (event) => {
   const { target } = event;
-
-  imagesRef.forEach((image) => {
-    if (image.getAttribute("alt") === target.alt) {
-      const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
     <div class="modal">
-    <img src='${image.getAttribute("data-source")}' alt='${image.getAttribute(
-        "alt"
-      )}'>
+    <img src='${target.getAttribute("data-source")}' alt='${target.getAttribute(
+    "alt"
+  )}'>
     </div>
 `);
-      const escapeListener = (escape) => {        
-        if (escape.key === "Escape") {
-          document.removeEventListener("keydown", escapeListener);
-          instance.close();
-        }
-      };
-      instance.show();
-      const modalEl = instance.element();
-      if (modalEl) {
-        modalEl.addEventListener("click", (event) => {
-          instance.close();
-        });
-        document.addEventListener("keydown", escapeListener);
-      }
-    }
+  instance.show((instance) => {
+    document.addEventListener("keydown", escapeListener);
   });
+  const escapeListener = (escape) => {
+    if (escape.key === "Escape") {
+      console.log(`done`);
+      instance.close(document.removeEventListener("keydown", escapeListener));
+    }
+  };
 };
 
 galleryRef.addEventListener("click", onImageClick);
+
+//   imagesRef.forEach((image) => {
+//     if (image.getAttribute("alt") === target.alt) {
+//       const instance = basicLightbox.create(`
+//     <div class="modal">
+//     <img src='${image.getAttribute("data-source")}' alt='${image.getAttribute(
+//         "alt"
+//       )}'>
+//     </div>
+// `);
+//       const escapeListener = (escape) => {
+//         if (escape.key === "Escape") {
+//           instance.close(
+//             document.removeEventListener("keydown", escapeListener)
+//           );
+//         }
+//       };
+//       instance.show((instance) => {
+//         const modalEl = instance.element();
+//         document.addEventListener("keydown", escapeListener);
+//         modalEl.addEventListener("click", (event) => {
+//           instance.close();
+//         });
+//       });
+//       // const modalEl = instance.element();
+//       // if (modalEl) {
+//       //   modalEl.addEventListener("click", (event) => {
+//       //     instance.close();
+//       //   });
+//       //   document.addEventListener("keydown", escapeListener);
+//       // }
+//     }
+//   });
 
 // const galleryRef = document.querySelector(".gallery");
 
@@ -70,18 +91,25 @@ galleryRef.addEventListener("click", onImageClick);
 //       )}'>
 //     </div>
 // `);
-//       instance.show();
-//       const modalEl = instance.element();
-//       if (modalEl) {
+//       instance.show((instance) => {
+//         const modalEl = instance.element();
 //         modalEl.addEventListener("click", (event) => {
-//           instance.close();
+//           instance.close((instance)=>{
+//             cl
+//           });
 //         });
-//         document.addEventListener("keydown", (escape) => {
-//           if (escape.key === "Escape") {
-//             instance.close();
-//           }
-//         });
-//       }
+//       });
+// const modalEl = instance.element();
+// if (modalEl) {
+//   modalEl.addEventListener("click", (event) => {
+//     instance.close();
+//   });
+//   document.addEventListener("keydown", (escape) => {
+//     if (escape.key === "Escape") {
+//       instance.close();
+//     }
+//   });
+// }
 //     }
 //   });
 // };
